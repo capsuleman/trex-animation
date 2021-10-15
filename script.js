7a0bd3759be6a1011f54e4e176da460cab27dcf0
Guillaume Vagner <guillaume.vagner@gmail.com>
2021-10-15 14:24:19 +0200

    :recycle: Animate with Green Socket

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const trex = document.getElementById("trex");
+gsap.to('#leg-1', { duration: 0.15, y: -4, repeat: -1, yoyo: true, ease: 'linear' })
+gsap.to('#leg-2', { duration: 0.15, y: -4, repeat: -1, yoyo: true, ease: 'linear', delay: 0.15 })
 
 trex.addEventListener('click', () => {
-    trex.classList.add('jump');
-
-    setTimeout(() => {
-        trex.classList.remove('jump');
-    }, 600)
+    gsap.timeline()
+        .to('#trex', { duration: 0.5, y: -80, ease: 'linear'})
+        .to('#trex', { duration: 0.5, y:   0, ease: 'linear'});
 })
